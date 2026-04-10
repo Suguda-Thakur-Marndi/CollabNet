8f128a74151fb11635d632af796a97f2a4f3385e
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-04-11 01:22:50 +0530

    feat: add user name input functionality to App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Editor from '@monaco-editor/react'
-import { useRef, useMemo, useEffect } from 'react'
+import { useRef, useMemo, useEffect, useState } from 'react'
 import * as Y from "yjs"
 import { SocketIOProvider } from "y-socket.io"
 import { MonacoBinding } from "y-monaco"
@@ -7,6 +7,7 @@ const App = () => {
   const editRef = useRef(null)
   const providerRef = useRef(null)
   const bindingRef = useRef(null)
+  const [userName, setUserName] = useState("")
   const ydoc = useMemo(() => new Y.Doc(), [])
   const ytext = useMemo(() => ydoc.getText('monaco'), [ydoc])
 
@@ -40,8 +41,23 @@ const App = () => {
 
     bindingRef.current = monacoBinding
   }
+  const handlejoin=(e)=>{
+    e.preventDefault()
+    
+  }
+  if(!userName)
+    return <main className='h-screen w-full p-4 bg-gray-950 flex gap-4 items-center justify-center'>
+      <form action=""></form>
+      <input
+      type='text'
+      placeholder='Enter your name'
+      value={userName}
+      onChange={(e) => setUserName(e.target.value)}
+      />
+    </main>
   return (
     <main className='h-screen w-full p-4 bg-gray-950 flex gap-4'> 
+    <form action="">
      <aside className='h-full w-1/4 rounded-lg bg-amber-50'>
      </aside>
      <section className='w-3/4 rounded-lg bg-neutral-600'>
@@ -52,7 +68,7 @@ const App = () => {
      onMount={handleMount}/>
 
      </section>
-
+</form>
      
     </main>
   )
