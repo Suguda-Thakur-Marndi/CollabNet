dd6bf80b25ed2e5b62f4ff10b98ce83b9a8034f3
Suguda-Thakur-Marndi <sugudathakurmarndi1106@gmail.com>
2026-05-16 22:34:04 +0530

    feat: implement Kit Token generation with environment variables for Videocall component

--- Frontend/src/Components/Videocall.jsx
+++ Frontend/src/Components/Videocall.jsx
@@ -26,8 +26,8 @@ export default function App() {
   let myMeeting = async (element) => {
 
  // generate Kit Token
- const appID = ;
- const serverSecret = "";
+ const appID =import.meta.env.VITE_APP_ID; ;
+ const serverSecret =import.meta.env.VITE_SERVER_SECRET; ;
  const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
 
  // Create instance object from Kit Token.
